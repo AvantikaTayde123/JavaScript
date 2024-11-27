b38ca039ae3d06900ec9abfdb0aa719f91f5105e
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-11-27 23:28:25 +0530

    Add files via upload

---
+++ Array6.js
@@ -0,0 +1,12 @@
+//remove last element of array
+arr=[4,5,6,7];
+//adding or removing elements from 1st index
+
+// arr.unshift(3);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+arr.length=arr.length-1;
+console.log(arr);
