0dc61c6d0689f43a0c6ce966212272a8a43ecc9d
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2025-01-23 23:51:42 +0530

    Create RemoveSpaces.js

---
+++ RemoveSpaces.js
@@ -0,0 +1,5 @@
+const sentence = "This is a simple sentence with spaces.";
+const result = sentence.split(' ').join('');
+
+console.log("Sentence without spaces:", result);
+// Output: "Thisisasimplesentencewithspaces."
