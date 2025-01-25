25893181a17896b7e664fecec6aceae9047bf055
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2025-01-25 23:29:58 +0530

    Create PatternExample2.js

---
+++ PatternExample2.js
@@ -0,0 +1,14 @@
+function printPyramid(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let spaces = ' '.repeat(rows - i);
+    let stars = '*'.repeat(2 * i - 1);
+    console.log(spaces + stars + spaces);
+  }
+}
+printPyramid(5);
+
+//     *    
+//    ***   
+//   *****  
+//  ******* 
+// *********
