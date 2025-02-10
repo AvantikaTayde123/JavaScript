37d3b3fc09a153d5c4bee1837147852723d9b8be
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2025-02-10 23:51:59 +0530

    Create GP.js

---
+++ GP.js
@@ -0,0 +1,16 @@
+function printGP(a, r, n) {
+    let series = [];
+    for (let i = 0; i < n; i++) {
+        series.push(a * Math.pow(r, i));
+    }
+    console.log("GP Series: " + series.join(" "));
+}
+
+function sumGP(a, r, n) {
+    return a * (1 - Math.pow(r, n)) / (1 - r);
+}
+
+// Example usage
+let a = 2, r = 3, n = 5;
+printGP(a, r, n);
+console.log("Sum of GP Series: " + sumGP(a, r, n));
