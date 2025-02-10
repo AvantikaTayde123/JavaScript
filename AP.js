a55497e104d3933c57c60b952ef52947aa887e7b
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2025-02-10 23:49:58 +0530

    Create AP.js

---
+++ AP.js
@@ -0,0 +1,16 @@
+function printAP(a, d, n) {
+    let series = [];
+    for (let i = 0; i < n; i++) {
+        series.push(a + i * d);
+    }
+    console.log("AP Series: " + series.join(" "));
+}
+
+function sumAP(a, d, n) {
+    return (n * (2 * a + (n - 1) * d)) / 2;
+}
+
+// Example usage
+let a = 2, d = 3, n = 5;
+printAP(a, d, n);
+console.log("Sum of AP Series: " + sumAP(a, d, n));
